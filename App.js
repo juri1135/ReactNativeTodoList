7204c1f268fee483e6c7ceb691c67e6bc4835206
juri1135 <juri113518@gmail.com>
2024-04-17 14:08:40 +0900

    Todolist divided into tasks and travel pages. Users can create and delete todolists.

--- App.js
+++ App.js
@@ -1,20 +1,275 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Appearance } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { light, dark } from './colors';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import { AntDesign, Feather, EvilIcons } from '@expo/vector-icons';
 
 export default function App() {
+  const screenHeight = Dimensions.get('window').height; // 사용자의 디바이스의 전체 화면 높이를 가져옴
+  const screenWidth = Dimensions.get('window').width;
+  const STORAGE_KEY = '@todos';
+  const [loading, setLoading] = useState(true); //loading중
+  const [userName, setUserName] = useState('');
+
+  const changeUserName = () => {
+    Alert.prompt(
+      '이름 입력', // 타이틀
+      '안녕하세요😺, 이름을 입력해주세요!', // 메세지
+      [
+        {
+          text: '싫어요😿',
+          style: 'cancel',
+        },
+        {
+          text: '여기용😽',
+          onPress: async (name) => {
+            setUserName(name);
+            await AsyncStorage.setItem('userName', name); // 이름 저장
+          },
+        },
+      ],
+      'plain-text' // 키보드 타입
+    );
+  };
+
+  //!---사용자가 테마 변경할 때마다 랜더링 ===>사용자의 테마에 따라서 어플의 테마 변경-----
+
+  const [appTheme, setAppTheme] = useState(light);
+
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(({ colorScheme }) => {
+      setAppTheme(colorScheme === 'dark' ? dark : light); // 시스템 테마 변경 감지
+    });
+    const loadUserName = async () => {
+      const storedUserName = await AsyncStorage.getItem('userName');
+      if (storedUserName) {
+        setUserName(storedUserName);
+      } else {
+        // 사용자 이름이 저장되어 있지 않으면 prompt를 통해 요청
+        if (!userName) {
+          changeUserName();
+        }
+      }
+    };
+    loadtodo();
+    loadUserName();
+    return () => subscription.remove(); // 컴포넌트 언마운트 시 리스너 제거
+  }, [userName]);
+
+  //!사용자가 체크박스 누를 때마다 checked 변경
+  const toggleCheckbox = (key) => {
+    const newToDos = {
+      ...todos,
+      [key]: { ...todos[key], checked: !todos[key].checked },
+    };
+    Savetodos(newToDos);
+  };
+
+  //!사용자가 x 누르면 todos 배열을 해당 todo key를 지운 배열로 update (상태 변경 동기화해서 그냥 원래 배열에서 key 지우면
+  //! 얘네는 바뀐 걸 모름. 무조건 새로운 배열 자체를 만들어서 다시 넣어줘야)
+  const deleteTodo = (key) => {
+    Alert.alert('삭제하시나요?', '정말로요?😿', [
+      {
+        text: '녜...😿',
+        onPress: async () => {
+          const newTodo = { ...todos };
+          delete newTodo[key];
+          setTodos(newTodo);
+          await Savetodos(newTodo);
+          Alert.alert('삭제되었어요...😿');
+        },
+      },
+      {
+        text: '아니용😸',
+        style: 'destructive',
+        onPress: () => {
+          Alert.alert('야호~😻');
+        },
+      },
+    ]);
+    return;
+  };
+
+  //!-------작업 페이지, 여행 페이지 설정---------
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  //!-----사용자가 텍스트 작성할 때마다 받아서 input에 저장해두기------------
+  const [input, setInput] = useState('');
+  const onChangeText = (payload) => setInput(payload);
+
+  //!JSON.stringify를 통해서 JSON 파일을 string으로 바꿀 수 있다.
+  //로컬 저장소에 todo list를 저장한다
+  const Savetodos = async (todo) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todo));
+      //!사용자가 check를 누르든 삭제를 하든 해서 상태가 바뀌면 저장하는데, 다른 페이지로 넘어가도 이 과정이
+      //!필요해서 저장한 뒤에는 todos 배열을 다시 setting해줘야 한다. 그래야 작업->여행 페이지 갔다 와도 상태 보존
+      setTodos(todo);
+    } catch {
+      alert('저장에 실패했습니다');
+    }
+  };
+
+  //!로컬 저장소에서 todo list를 가져온다.
+  //string으로 저장했기 때문에 얘네를 다시 object로 바꿔줘야 함
+  //사용자가 이전에 입력한 게 없다면 s가 null이라서 parse 불가! =>null이면 따로 setting해주어야 한다
+  const loadtodo = async () => {
+    try {
+      const s = await AsyncStorage.getItem(STORAGE_KEY);
+      s === null ? setTodos({}) : setTodos(JSON.parse(s));
+      setLoading(false);
+    } catch {
+      alert('저장소를 읽어들이는 데 실패했습니다');
+    }
+  };
+
+  //!-----------사용자가 전송 누르면 지금까지 쌓았던 input을 todos hashmap에 저장하기----------
+  const [todos, setTodos] = useState({});
+  //local 저장소에 계속 동기화해야 하기 때문에 async
+  const addToDo = async () => {
+    if (input === '') return;
+    //새로운 newToDos hashmap은 새로운 newToDos {}배열에 기존 todos 배열에다가 object를 하나 추가한다.
+    //key는 Date.now()로 설정하고 value는 input(할 일), work(작업인지 여행인지)
+    //const newToDos = Object.assign({}, todos, { [Date.now()]: { input, work: working } });
+    //stylesheet에서 사용하는 ...방식으로도 가능!
+    const newToDos = { [Date.now()]: { input, working, checked: false }, ...todos };
+    //hashmap에 모든 todo 넣어주기
+    setTodos(newToDos);
+    //local에 저장
+    await Savetodos(newToDos);
+    //다음에 받을 input은 다시 ""로 초기화해주기
+    setInput('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={appTheme}>
+      {loading ? (
+        <AntDesign
+          name="loading1"
+          size={24}
+          color={appTheme.container}
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        />
+      ) : (
+        <View style={[styles.container, { paddingTop: screenHeight * 0.04, backgroundColor: appTheme.bg }]}>
+          <StatusBar style="auto" />
+          <View style={styles.header}>
+            <TouchableOpacity onPress={work}>
+              <Text style={{ ...styles.btnTxt, color: working ? appTheme.blue : appTheme.grey }}>작업 📚</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={travel}>
+              <Text style={[styles.btnTxt, { color: !working ? appTheme.blue : appTheme.grey }]}>여행🧳</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={{ textAlign: 'right', marginRight: 20, marginTop: 15 }}>🐈</Text>
+          <TextInput
+            returnKeyType="done"
+            style={{ ...styles.input, backgroundColor: appTheme === light ? '#B1B3B0' : light.bg }}
+            placeholder={working ? 'What to do?' : 'Where do you want to go?'}
+            value={input}
+            placeholderTextColor="#565656"
+            //여기서 text 바뀔 때마다 이 함수 호출 안 해주면 input 저장이 안 됨 return하기 전까지 일단 쌓아두고
+            onChangeText={onChangeText}
+            //return하면 그 값을 todos hashmap에 넘기는 거임
+            onSubmitEditing={addToDo}
+          />
+          {userName === '' ? null : (
+            <Text
+              style={{
+                textAlign: 'center', // 중앙 정렬
+                color: appTheme === light ? dark.grey : light.bg,
+              }}
+            >
+              ---------{userName}'s To-do List😺---------
+              <TouchableOpacity onPress={() => changeUserName()}>
+                <EvilIcons name="pencil" size={24} color={appTheme === light ? dark.bg : light.bg} />
+              </TouchableOpacity>
+            </Text>
+          )}
+
+          <ScrollView>
+            {Object.keys(todos).map((key) =>
+              //사용자가 작업을 누르고 작성하면 그 때 working은 작업이라서 hashmap에 working이 작업으로 들어감
+              //여기서 todolist를 보여줄 때 현재 있는 테마랑 같으면 보여주고 아니면 보여주지 마라
+              //=>working이면 working만 보여줌
+              todos[key].working === working ? (
+                <View key={key} style={{ ...styles.todo, backgroundColor: appTheme.container }}>
+                  <BouncyCheckbox
+                    isChecked={todos[key].checked}
+                    onPress={() => toggleCheckbox(key)}
+                    size={20}
+                    fillColor={appTheme === light ? '#4287F5' : '#E3AC00'}
+                    unfillColor="#FFFFFF"
+                    iconStyle={{
+                      borderRadius: 0,
+                      marginRight: -16,
+                    }}
+                    innerIconStyle={{
+                      borderRadius: 0, // to make it a little round increase the value accordingly
+                    }}
+                  />
+                  <Text
+                    style={{
+                      flex: 1,
+                      textDecorationLine: todos[key].checked ? 'line-through' : 'none',
+                      color: light.bg,
+                      fontSize: 16,
+                      fontWeight: '500',
+                      textAlign: 'center',
+                    }}
+                  >
+                    {todos[key].input}
+                  </Text>
+                  <TouchableOpacity onPress={() => deleteTodo(key)}>
+                    <Feather name="trash-2" size={18} color={light.bg} />
+                  </TouchableOpacity>
+                </View>
+              ) : null
+            )}
+          </ScrollView>
+        </View>
+      )}
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+  },
+  header: {
+    paddingTop: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  btnTxt: {
+    fontSize: 38,
+    fontWeight: '600',
+  },
+  input: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  todo: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 10,
+    //backgroundColor: appTheme.container, // Theme container color
+    borderRadius: 10,
+    height: 55,
   },
 });
---
+++ colors.js
@@ -0,0 +1,12 @@
+export const light = {
+  bg: '#FCFDFF',
+  grey: '#D2D5DE',
+  blue: '#94BDF7',
+  container: '#8EB5EE',
+};
+export const dark = {
+  bg: 'black',
+  grey: '#565656',
+  blue: '#466BB8',
+  container: '#466BB8',
+};
